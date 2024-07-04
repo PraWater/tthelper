23e1b208ef7d8799c4b9d523072a36b6e034518a
PraWater <praneel.maddula@gmail.com>
2024-07-04 12:36:52 +0530

    add: parse slot

--- internal/input/input.go
+++ internal/input/input.go
@@ -2,7 +2,9 @@ package input
 
 import (
 	"bufio"
+	"errors"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -19,7 +21,7 @@ func ReadFile(filename string) (sections [][]string, err error) {
 		fields := strings.Fields(line)
 
 		for i := 2; i < len(fields); i++ {
-      sections = append(sections, []string{strings.Join(fields[:2], " "), fields[i]})
+			sections = append(sections, []string{strings.Join(fields[:2], " "), fields[i]})
 		}
 	}
 
@@ -27,5 +29,48 @@ func ReadFile(filename string) (sections [][]string, err error) {
 		return nil, err
 	}
 
+	return
+}
+
+func ParseSlot(slot string) (times [][]int, err error) {
+	splitSlots := strings.Split(slot, " ")
+	previousNum := false
+	currentDays := []int{}
+	for _, splits := range splitSlots {
+		s := strings.Trim(splits, " ")
+		if len(s) == 0 {
+			continue
+		}
+		if time, e := strconv.Atoi(s); e == nil {
+      previousNum = true
+      for _, day := range currentDays {
+        times = append(times, []int{day, time})
+      }
+		} else {
+      if previousNum {
+        currentDays = nil
+      }
+      previousNum = false
+      day := 0
+      switch s {
+      case "M":
+        day = 0
+      case "T":
+        day = 1
+      case "W":
+        day = 2
+      case "Th":
+        day = 3
+      case "F":
+        day = 4
+      case "S":
+        day = 5
+      default:
+        err = errors.New("Unexpected string for day")
+        return
+      }
+      currentDays = append(currentDays, day)
+    }
+	}
 	return
 }
--- internal/input/input_test.go
+++ internal/input/input_test.go
@@ -1,20 +1,73 @@
 package input_test
 
 import (
+	"reflect"
 	"testing"
-  "reflect"
 
 	"github.com/PraWater/tthelper/internal/input"
 )
 
 func TestInput(t *testing.T) {
-  got, err := input.ReadFile("input_test.txt")
+	got, err := input.ReadFile("input_test.txt")
 	want := [][]string{{"BIO F215", "L1"}, {"BIO F215", "T1"}, {"BIO F231", "L1"}}
 
-  assertNoError(t, err)
+	assertNoError(t, err)
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
 	}
+}
+
+func TestParseSlot(t *testing.T) {
+	cases := []struct {
+		Name   string
+		Input  string
+		Output [][]int
+	}{
+		{
+			"Single day with one time slot",
+			"Th  1",
+			[][]int{{3, 1}},
+		},
+		{
+			"Multiple days with same time slot",
+			"M W F  5",
+			[][]int{{0, 5}, {2, 5}, {4, 5}},
+		},
+		{
+			"Multiple days with different time slot",
+			"M  5  T  4",
+			[][]int{{0, 5}, {1, 4}},
+		},
+		{
+			"Single day with multiple time slot",
+			"T  4 5",
+			[][]int{{1, 4}, {1, 5}},
+		},
+		{
+			"Multiple days with multiple time slot",
+			"T  4 5  S  7 8",
+			[][]int{{1, 4}, {1, 5}, {5, 7}, {5, 8}},
+		},
+	}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+      got, err := input.ParseSlot(test.Input)
+
+      assertNoError(t, err)
+			if !reflect.DeepEqual(got, test.Output) {
+				t.Errorf("got %v, want %v", got, test.Output)
+			}
+		})
+	}
+
+  t.Run("Unexpected day character", func(t *testing.T) {
+    slot := "P  7"
+    _, err := input.ParseSlot(slot)
+
+    if err == nil {
+      t.Errorf("Expected error when giving %q as input\n", slot)
+    }
+  })
 }
 
 func assertNoError(t testing.TB, err error) {
